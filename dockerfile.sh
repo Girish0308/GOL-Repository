258ee00f5e24a389e2f0bf5b519dd85d28580fa5
Girish0308 <62947744+Girish0308@users.noreply.github.com>
2020-04-17 11:33:08 +0530

    v3.0 Release

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,4 +1,6 @@
-cd /tmp/workspace/CICD-Jenkinfile
+cd /tmp/workspace/CICD-Jenkinfile/jenkins-deploy
+cp /tmp/workspace/CICD-Jenkinfile/Dockerfile . 
+
 docker build -t mydeploy:$BUILD_NUMBER .
 docker run -d -P mydeploy:$BUILD_NUMBER
                                     
