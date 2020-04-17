58ca7cc7308062e479415c08302e1f18c2eeb242
Girish0308 <62947744+Girish0308@users.noreply.github.com>
2020-04-17 10:59:06 +0530

    file update

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,14 @@ mkdir jenkins-deploy
 cd jenkins-deploy
 cp /tmp/workspace/CICD-Jenkinfile/gameoflife-web/target/gameoflife.war .
 
+cat <<EOT>> Dockerfile
+
+FROM tomcat:8.5.16-jre8-alpine
+ADD gameoflife.war /usr/local/tomcat/webapps
+CMD "catalina.sh" "run"
+EXPOSE 8080
+EOT
+
+docker build -t mydeploy:$BUILD_NUMBER .
+docker run -d -P mydeploy:$BUILD_NUMBER
+
